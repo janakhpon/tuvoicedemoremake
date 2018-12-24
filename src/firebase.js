6d7b611a5a28ab12df54dc333b3301f6ec9cc0c6
novae <jnovaxer@gmail.com>
2018-12-24 15:39:13 +0630

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,7 @@
 import * as firebase from 'firebase';
 
 var config = {
-    apiKey: "AIzaSyBTveQxsLYdWfI3w6uG225gEAWrwkMrQng",
-    authDomain: "tuvoicedemo.firebaseapp.com",
-    databaseURL: "https://tuvoicedemo.firebaseio.com",
-    projectId: "tuvoicedemo",
-    storageBucket: "tuvoicedemo.appspot.com",
-    messagingSenderId: "1045598113918"
+
 }
 
 firebase.initializeApp(config);
